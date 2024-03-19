b1ec7479ff06a0e98ffc69faf290749a2d3db7a5
Jialin Wang <choumingzhao@outlook.com>
2024-03-19 17:11:35 +0800

    add visit counts using session

--- catalog/views.py
+++ catalog/views.py
@@ -17,13 +17,18 @@ def index(request):
     num_genres = Genre.objects.count()
     
     num_books_with_word = Book.objects.filter(title__icontains='of').count()
+    
+    # visit counts
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
-        'num_books_with_word': num_books_with_word
+        'num_books_with_word': num_books_with_word,
+        'num_visits': num_visits
     }
     return render(request, 'index.html', context=context)
 
